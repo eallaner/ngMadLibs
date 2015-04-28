1c5a4460d554e047969747d6d57eccca161676fc
eallaner <luciawang0402@gmail.com>
2015-04-27 21:55:09 -0700

    Final Version of Mad Lib app

--- myApp.js
+++ myApp.js
@@ -1,16 +1,43 @@
 // JavaScript Document
-angular.module('myApp',[])
+angular.module('myApp',['ngMessages'])
   .controller('myCtrl', ['$scope', function($scope) {
        $scope.sexes = ['Female', 'Male'];
        $scope.selection = $scope.sexes[0];
+       $scope.switchViews = true;
 	   
-	   $scope.name1='Name';
-	   $scope.name2='Job Title';
-	   $scope.name3='Tedious Task';
-	   $scope.name4='Dirty Task';
-	   $scope.name5='Celebrity';
-	   $scope.name6='Useless Skill';
-	   $scope.name7='Obnoxious Task';
-	   $scope.name8='Huge Number';
-	   $scope.name9='Objective';
+	   $scope.name1='';
+	   $scope.name2='';
+	   $scope.name3='';
+	   $scope.name4='';
+	   $scope.name5='';
+	   $scope.name6='';
+	   $scope.name7='';
+	   $scope.name8='';
+	   $scope.name9='';
+	   
+	   $scope.submit = function(){
+	            $scope.name1=this.name1;
+	   			$scope.name2=this.name2;
+	   			$scope.name3=this.name3;
+	   			$scope.name4=this.name4;
+	   			$scope.name5=this.name5;
+	   			$scope.name6=this.name6;
+	   			$scope.name7=this.name7;
+	   			$scope.name8=this.name8;
+	   			$scope.name9=this.name9;
+	            $scope.switchViews = false;
+	   };
+       
+	   $scope.reset = function(){
+        	    $scope.name1='';
+	   			$scope.name2='';
+	   			$scope.name3='';
+	   			$scope.name4='';
+	    	    $scope.name5='';
+	   			$scope.name6='';
+	   			$scope.name7='';
+	   			$scope.name8='';
+	   			$scope.name9='';
+        	    $scope.switchViews = true;
+       };
   }]);
